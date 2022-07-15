87765fb34f6f9be142266f6c203a184a89037187
jorgeruiz27 <jorge.cx.27@gmail.com>
2022-07-15 16:39:41 -0400

    Adicionado models com tabelas de pessoas e atividades e criado um utils de teste para manipular os dados do banco de dadeos

---
+++ utils.py
@@ -0,0 +1,27 @@
+from models import Pessoas
+
+def insere_pessoa():
+    pessoa = Pessoas(nome='Luiz', idade=40)
+    print(pessoa)
+    pessoa.save()
+
+def consulta_pessoa():
+    #pessoa = Pessoas.query.filter_by(nome='Luiz').first()
+    pessoa = Pessoas.query.all()
+    print(pessoa)
+    #print(pessoa.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Luiz').first()
+    pessoa.idade = 50
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Luiz').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoa()
+    #altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoa()
